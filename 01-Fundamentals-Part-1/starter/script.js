78f39eb1f441d008c387f9b287054f8b5d357ac5
cep104 <candicelizabethpeters@gmail.com>
2020-11-25 15:30:07 -0600

    dataType done

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,10 @@
+// variable assignment
 let country = "United States"
 let continent = "North America"
 let population = 3282000000
 console.log(country, continent, population)
+
+// dataType assignment
+let isIsland = false;
+let language;
+console.log(country, population, isIsland, language)
