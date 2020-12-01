2a70e788cfbd1e7d1edfb56f2ff3c350e17c3032
cep104 <candicelizabethpeters@gmail.com>
2020-11-30 20:51:30 -0600

    challenge 3

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+
 // variable assignment
 let country = "United States"
 let continent = "North America"
@@ -41,8 +42,52 @@ let markHigherBMI = markBmi > johnBmi
 console.log(markHigherBMI)
 
 // coding challenge 2
+
 if(markHigherBMI)
 
 console.log(`Mark's (${markBmi}) is higher than John's!`)
 else
 console.log(`John's (${johnBmi}) is higher than Mark's!`)
+
+// coding challenge 3
+
+// Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+// Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+// Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+
+let dolphinsScore = (96 + 108 + 89)/ 3 
+let koalasScore = (88 + 91 + 110)/ 3
+
+console.log(dolphinsScore, koalasScore)
+
+if(dolphinsScore > koalasScore){
+    console.log(dolphinsScore)
+}
+if(koalasScore > dolphinsScore){
+    console.log(koalasScore)
+}
+if(koalasScore === dolphinsScore){
+    console.log(`it's a draw`)
+}
+
+dolphinsScore = (97 + 112 + 101)/ 3 
+ koalasScore = (109 + 95 + 123)/ 3
+
+ if(dolphinsScore > koalasScore && dolphinsScore > 100){
+    console.log(`Dolphins win with a score of: ${dolphinsScore}`)
+ }else if(koalasScore > dolphinsScore && koalasScore > 100){
+    console.log(`Koalas win with a score of: ${koalasScore}`)
+    }else{
+       console.log(`no one wins`) 
+    }
+
+    dolphinsScore = (97 + 112 + 101)/ 3 
+    koalasScore = (109 + 95 + 106)/ 3
+
+    if(dolphinsScore > koalasScore && dolphinsScore > 100){
+        console.log(`Dolphins win with a score of: ${dolphinsScore}`)
+     }else if(koalasScore > dolphinsScore && koalasScore > 100){
+        console.log(`Koalas win with a score of: ${koalasScore}`)
+        }else if(dolphinsScore === koalasScore && koalasScore > 100 && dolphinsScore > 100){
+           console.log(`it's a draw koalas: ${koalasScore}, dolphins: ${dolphinsScore}`) 
+        }
